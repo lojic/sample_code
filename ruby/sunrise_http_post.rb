f89b71aeadd3e179a34981d49fb295264be2af76
Brian Adkins <info@lojic.com>
2009-03-31 20:57:06 -0400

    fixed problem with date arithmetic

--- ruby/sunrise_http_post.rb
+++ ruby/sunrise_http_post.rb
@@ -1,15 +1,9 @@
 #!/usr/local/bin/ruby
-#
 # An example of using HTTP POST to retrieve sunrise/set data
+
+# From: http://aa.usno.navy.mil/data/docs/RS_OneDay.php
 #
-# Author : Brian Adkins
-# Site   : http://lojic.com
-# License: Just leave copyright notice :)
-# Copyright 2009 Brian Adkins, all rights reserved
-#
-# Reference: http://aa.usno.navy.mil/data/docs/RS_OneDay.php
-#
-# "If you plan to write your own form to access the cgi script for
+# If you plan to write your own form to access the cgi script for
 # sunrise/sunset calculations (not recommended), please use the above
 # line in your form but substitute in the value field an identifier of
 # your own choosing, up to 8 characters.  For example, <input
@@ -20,30 +14,21 @@
 # usage log, and helps us justify our work on the web.  (Nothing in this
 # note should be construed as encouragement to set up your own form, as
 # we do not guarantee that either our form or the program called by the
-# cgi script will not change.)"
+# cgi script will not change.)
 
-# Here's a simple Emacs function to invoke this Ruby script if it's
-# executable and available in the path
-# (defun bja-sunrise ()
-#   "Display sunrise/sunset information."
-#   (interactive)
-#   (shell-command "sunrise_http_post.rb"))
-
+require 'date'
 require 'net/http'
 
 YOUR_ID    = ''    # A unique ID per comment above
 YOUR_CITY  = ''    # The name of your city
 YOUR_STATE = ''    # Two letter state abbreviation
 
-now   = Time.now
-month = now.month
-day   = now.day + 1 # Tomorrow
-year  = now.year
+date = DateTime.now + 1 # tomorrow
 
 Net::HTTP.start('aa.usno.navy.mil') do |query|
-  response = query.post('/cgi-bin/aa_pap.pl', "FFX=1&ID=#{YOUR_ID}&xxy=#{year}&xxm=#{month}&xxd=#{day}&st=#{YOUR_STATE}&place=#{YOUR_CITY}&ZZZ=END")
+  response = query.post('/cgi-bin/aa_pap.pl', "FFX=1&ID=#{YOUR_ID}&xxy=#{date.year}&xxm=#{date.month}&xxd=#{date.day}&st=#{YOUR_STATE}&place=#{YOUR_CITY}&ZZZ=END")
   if response.body =~ /Begin civil twilight[^0-9]*(\d+:\d{2} [ap].m.).*Sunrise[^0-9]*(\d+:\d{2} [ap].m.).*Sunset[^0-9]*(\d+:\d{2} [ap].m.).*End civil twilight[^0-9]*(\d+:\d{2} [ap].m.)/m
-    puts "#{month}/#{day}/#{year}"
+    puts "#{date.month}/#{date.day}/#{date.year}"
     puts "Begin Twilight: #{$1}"
     puts "Sunrise       : #{$2}"
     puts "Sunset        : #{$3}"
