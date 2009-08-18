c42a473a58eb5d0d90ad79b004cf02e5d9a0849e
Brian Adkins <info@lojic.com>
2009-08-18 15:24:32 -0400

    add Emacs timelog hierarchical viewer

---
+++ ruby/timelog_tree.rb
@@ -0,0 +1,29 @@
+#!/usr/local/bin/ruby
+PREFIX_LEN = '  '
+
+def each_description file
+  file.each_line {|line| yield line.split(' ')[3..-1] if line[0,1] == 'i' }
+end
+
+def update_categories cat, tags
+  return if tags.empty?
+  cat[tags[0]] = {} if cat[tags[0]].nil?
+  update_categories(cat[tags[0]], tags[1..-1])
+end
+
+def print_categories cat, depth, prefix=''
+  return if cat.empty?
+  cat.keys.sort.each do |k|
+    puts prefix + k
+    print_categories(cat[k], depth-1, prefix+PREFIX_LEN) if depth > 1
+  end
+end
+
+def process_file file, cat
+  each_description(file) do |tags|
+    update_categories(cat, tags)
+  end
+  cat
+end
+
+print_categories(process_file(STDIN, {}), ARGV.length == 1 ? ARGV[0].to_i : 99)
